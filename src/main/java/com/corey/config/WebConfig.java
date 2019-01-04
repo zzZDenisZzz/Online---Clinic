cafd13b1afe6e15e557ecef56615d315a920b926
zzZDenysZzz <denys.zazeka@gmail.com>
2019-01-04 16:03:35 +0200

    Created WebConfig

---
+++ src/main/java/com/corey/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.corey.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.corey.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setOrder(1);
+        freeMarkerViewResolver.setSuffix(".flt");
+        freeMarkerViewResolver.setPrefix("");
+        return freeMarkerViewResolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer getFreeMarkerConfigurer() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/");
+        return freeMarkerConfigurer;
+    }
+
+}
