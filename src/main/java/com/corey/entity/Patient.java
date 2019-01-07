9c8c78257d9a563e526e0c0113bb3d15996c5677
zzZDenysZzz <denys.zazeka@gmail.com>
2019-01-07 19:31:22 +0200

    created hibernate config and added dao

--- src/main/java/com/corey/entity/Patient.java
+++ src/main/java/com/corey/entity/Patient.java
@@ -4,18 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "patients")
 public class Patient {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    private int age;
     private String email;
     private String address;
-    private int age;
+    @Column(name = "data_of_visit")
     private String dateOfVisit;
     private String illness;
-
 }
---
+++ src/main/java/com/corey/utils/HibernateSessionFactoryUtil.java
@@ -0,0 +1,30 @@
+package com.corey.utils;
+
+import com.corey.entity.Patient;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+@Slf4j
+public class HibernateSessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactoryUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(Patient.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                log.info("Error: " + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
